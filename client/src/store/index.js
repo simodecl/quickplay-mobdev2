95c8ef168cbff89c2166ff16213716cfdc661f40
simodecl <simodecl@student.arteveldehs.be>
2018-06-03 12:59:44 +0200

    heroku

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,14 +5,17 @@ import rootReducer from '../reducers';
 const initialState = {
 };
 const middleware = [thunk];
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
 );
+const store = createStore(rootreducer, enhancer);
 
 export default store;
